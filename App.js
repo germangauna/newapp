c16c5b2c5ba11a6bd2a49eed97a2ad93bbb8229c
German Gauna <germangauna88@gmail.com>
2023-02-23 20:08:10 -0300

    core component

--- App.js
+++ App.js
@@ -1,21 +1,47 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hola Coder</Text>
-      <StatusBar style="auto" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Item de Lista" style={styles.Input} />
+        <Button title="Agregar"/>
+      </View>
+        <View style={styles.itemsContainer}>
+        <Text style={styles.item}> texto 1 </Text>
+        <Text style={styles.item}> texto 2 </Text>
+        <Text style={styles.item}> texto 3 </Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  screen:{
+      padding:30,
+  },
+  inputContainer:{
+        marginTop: 30,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems:"center",
+        
+  },
+ Input:{
+          width: 200,
+          height:30,
+          borderBottomColor:"black",
+          borderBottomWidth: 1,
+  },
+  itemsContainer: {
+   marginTop:30,
+  },
+  item: {
+    padding: 10,
+    textAlign: 'center',
+    color: 'green',
+    fontFamily: '',
   },
 });
