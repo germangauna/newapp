73d58ddb0eaa9444d7061f9869b20a843c407034
German Gauna <germangauna88@gmail.com>
2023-03-01 00:57:00 -0300

    agregamos modal

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, FlatList, Modal, Pressable, StyleSheet, Text, TextInput, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 
 import { StatusBar } from 'expo-status-bar';
@@ -7,42 +7,80 @@ export default function App() {
 
   const [itemText, setItemText] = useState('');
   const [items, setItems] = useState([]);
-
-  useEffect(() => {
-    console.log('items', items);
-  }, [items]);
   
   const onChangeText = (text) => {
     setItemText(text);
   };
-  
+
   const addItem = () => {
     setItems(oldArry => [...oldArry, {id: Date.now(), value: itemText }]);
     setItemText('');
   };
 
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedItem, setSelectedItem] = useState(null);
+
+  const removeItem = (id) => {
+    setModalVisible(!modalVisible);
+    setItems((oldArry) => oldArry.filter((item) => item.id !== id));
+    setSelectedItem(null);
+  };
+
+  const selectItem = (item) => {
+    setSelectedItem(item);
+    setModalVisible(true);
+  };
 
   return (
     <View style={styles.screen}>
       <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Item de Lista"
-          style={styles.Input}
-          onChangeText={onChangeText}/>
-        <Button
-          title="Agregar"
-          onPress={addItem}  />
+              <TextInput
+              placeholder="Item de Lista"
+              style={styles.Input}
+              onChangeText={onChangeText}/>
+              <Button
+              title="Agregar"
+              onPress={addItem}  />
       </View>
       <View style={styles.itemsContainer}>
-        {/* {items.map(item => <Text key={item.id} style={styles.item}>{item.value}</Text>)} */}
         <FlatList
-          data={items}
-          renderItem={(itemData) => (
-            <View stile={stiles.contentList}>
-              <Text>{itemData.item.value}</Text>
-              <Button title='delete' onPress={() => onHandleDelete(intemData.item.id)}></Button>
-          </View> ) } >
+              data={items}
+              renderItem={(itemData) => (
+              <Pressable stile={styles.contentList} onPress={() => {
+              selectItem(itemData.item)
+              }}>
+              <Text stile={styles.item}>{itemData.item.value}</Text>
+            </Pressable>)} keyExtractor = {(item)  => item.id.toString()} >
         </FlatList>
+        <Modal
+              animacionType='slider'//mirar animaciones en la doc variedades
+              transparent={true}
+              visible={modalVisible}>
+           <View style={styles.modalContainer}>    
+          <View tyle={styles.modaltitle}>
+            <Text style={{
+              textAlign: 'center',
+              fontSize: 20,
+              fontWeight: 'bold',
+            }}>eliminar Item</Text>
+          </View>
+          <View style={styles.modalContent}>
+              <View >
+              <Text>esta seguro de que quiere eliminar el items {selectItem?.    value}?</Text>
+              {/* //el signo . value  es por las dudas que tengas un error */}
+              </View>
+              <View tyle={styles.modalAction}>
+              <Button title='CANCELAR' onPress={() => { 
+              setModalVisible(false)
+              setSelectItem(null);
+              }}></Button>
+              <Button title='ELIMINAR' onPress={() => { 
+              removeItem(selectItem.id)
+                }}></Button>
+                </View> 
+            </View>
+          </View>
+        </Modal>
       </View>
     </View>
   );
@@ -74,4 +112,33 @@ const styles = StyleSheet.create({
     color: 'green',
     fontFamily: '',
   },
+  contentList: {
+    padding: 10,
+    borderRadius: 5,
+    backgroundColor: '#ccc',
+  },
+  modalContainer: {
+    height: 250,
+    width: 250,
+    marginTop: 100,
+    alignSelf: 'center',
+    backgroundColor:'red',
+  },
+  modaltitle: {
+    backgroundColor: '#ccc',
+    padding: 10,
+    borderRadius: 5,
+  },
+  modalContent: {
+    padding: 10,
+    width: '50%',
+  },
+  modalAction: {
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+  },
+  modalAction: {
+   textAlign:'center',
+  },
+  
 });
